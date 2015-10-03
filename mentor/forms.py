fa404452f77b3756e2a54df75c6503cae697e118
Jon Wood <jon@blankpad.net>
2015-10-03 14:38:15 +0100

    Copy email address to username

--- mentor/forms.py
+++ mentor/forms.py
@@ -4,14 +4,15 @@ from django.contrib.auth.forms import UserCreationForm
 from mentor.models import UserProfile
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("email", "password1", "password2")
 
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
+        user.username = self.cleaned_data["email"]
         user.email = self.cleaned_data["email"]
 
         if commit:
