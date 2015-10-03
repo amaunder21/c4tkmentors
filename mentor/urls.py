29d3010105ab3565dd1a416fd64f05b9044cca98
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2015-10-03 12:20:01 +0100

    extended registration from for email address

--- mentor/urls.py
+++ mentor/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'/register', views.register, name='register'),
+	url(r'lostpass', views.lostpass, name='lostpass'),
+    url(r'register', views.register, name='register'),
+    url(r'success', views.success, name='success'),
     url(r'^$', views.index, name='index'),
 ]
--- mentor/views.py
+++ mentor/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from forms import RegistrationForm
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Mentor index.")
 
 
-
 def register(request):
-	return HttpResponse("register.")
-	#
-    #if request.method == 'POST':
-    #    form = UserCreationForm(request.POST)
-    #    if form.is_valid():
-    #        new_user = form.save()
-    #        return HttpResponseRedirect("/books/")
-    #else:
-    #    form = UserCreationForm()
-    #return render(request, "registration/register.html", {
-    #    'form': form,
-    #})
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/success")
+    else:
+        form = RegistrationForm()
+    return render(request, "registration/register.html", {
+        'form': form,
+	})
+
+
+
+def lostpass(request):
+	return HttpResponse("lostpass.")
+
+def success(request):
+	return HttpResponse("Successful registration.")
+
+	
+
+
+
+
+
+
+
+
+
+
 
 # Create your views here.
